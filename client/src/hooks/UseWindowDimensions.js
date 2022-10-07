63cfc628a7f7275859192fc73841ed00111d1ace
DanielDafnis <danieldafnis@gmail.com>
2022-10-07 16:46:17 +0300

    images in db- fixed responsiveness of landing page

---
+++ client/src/hooks/UseWindowDimensions.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+
+function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height} = window;
+    return {
+        width,
+        height
+    };
+}
+
+export default function useWindowDimensions() {
+    const [WindowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    useEffect(() => {
+    function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+    }
+    
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return WindowDimensions;
+}
