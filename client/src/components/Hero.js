22be385e4fc08c5f7fc6c20e8ada8f65b5fb94d7
TehShadow <ageldim.main@gmail.com>
2022-11-01 13:07:52 +0200

    video

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -5,7 +5,7 @@ function Hero(){
     return(
             <div className="video-wrapper">
                     <video autoPlay={true} muted={true} loop>
-                        <source src="/Media/Site_teliko.mp4" type="video/mp4"/>
+                        <source src="/Media/hero_video.mp4" type="video/mp4"/>
                         Your browser does not support the video tag.
                     </video>
 
