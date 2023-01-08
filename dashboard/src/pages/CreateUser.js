2481549ca2cb7706f32df4e6375919033b0d9e37
DanielDafnis <danieldafnis@gmail.com>
2023-01-08 04:28:24 +0200

    fix

--- dashboard/src/pages/CreateUser.js
+++ dashboard/src/pages/CreateUser.js
@@ -1,5 +1,7 @@
 import React , { useState } from "react"
-import { Form } from "react-router-dom"
+import  Form  from "react-bootstrap/Form"
+import Button from "react-bootstrap/esm/Button"
+import axios from "axios"
 
 
 function CreateUser (){
