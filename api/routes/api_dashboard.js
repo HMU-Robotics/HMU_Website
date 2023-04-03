50d1454a9ca2f7d9bc259efdaa3c3d78039d0ef9
DanielDafnis <danieldafnis@gmail.com>
2023-04-04 02:43:11 +0300

    fix maybe?

--- api/routes/api_dashboard.js
+++ api/routes/api_dashboard.js
@@ -5,11 +5,12 @@ const {admin,viewer,editor} = require("../middleware/roles")
 const dashboard_controller = require("../controllers/dashboard")
 const check_session = require("../middleware/session")
 const { uploadImages , resizeImages  ,makePost ,makeMember} = require("../controllers/uploadController")
+const uploadController = require('../controllers/uploadController')
 
 
 ////////////////// Posts //////////////////////////////
 
-router.post('/addPost',AuthMiddleware,viewer,check_session,makePost)
+router.post('/addPost',AuthMiddleware,viewer,check_session,uploadController.makePost)
 router.put('/editPost',AuthMiddleware,viewer,check_session,dashboard_controller.post_post)
 
 /////////////////// Members //////////////////////////
