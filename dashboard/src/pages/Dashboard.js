3e3ddfec866c4634c248f89233f1a837e6770902
DanielDafnis <danieldafnis@gmail.com>
2023-04-07 01:07:43 +0300

    postman worked

--- dashboard/src/pages/Dashboard.js
+++ dashboard/src/pages/Dashboard.js
@@ -4,6 +4,9 @@ import { useNavigate } from "react-router-dom"
 
 function Dashboard () {
 
+    const api_url = "https://robotics-club.hmu.gr:443/api/dashboard/test"
+    const emailTest = "test123"
+
     const navigate = useNavigate();
 
     const gotoCreatePost = () => {
@@ -22,6 +25,18 @@ function Dashboard () {
         navigate("/edituser")
     }
 
+    const gotoTest = async(e) => {
+        await axios.post(api_url, {
+            "email": emailTest
+        })
+        .then((res) => {
+            console.log(res)
+            console.log(res.status)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 
     return(
         <>
@@ -41,6 +56,10 @@ function Dashboard () {
             <div className="wrapper">
                 <h3>Edit User</h3>
                 <Button onClick={gotoEditUser} variant="primary">Button</Button>
+            </div>
+            <div className="wrapper">
+                <h3>test</h3>
+                <Button onClick={gotoTest} variant="primary">Button</Button>
             </div>
         </>
     )
