7d6a3f5e98f00f6fc4600bfc7cbb6599aba3022d
DanielDafnis <danieldafnis@gmail.com>
2023-04-07 02:35:51 +0300

    ..

--- api/routes/api_dashboard.js
+++ api/routes/api_dashboard.js
@@ -12,7 +12,7 @@ const { uploadImages , resizeImages  ,makePost ,makeMember, test} = require("../
 router.post('/addPost',AuthMiddleware,viewer,check_session,uploadImages,resizeImages,makePost)
 router.put('/editPost',AuthMiddleware,viewer,check_session,dashboard_controller.post_post)
 // this is a test will get deleted
-router.post('/test',viewer,test)
+router.post('/test',AuthMiddleware,viewer,test)
 
 /////////////////// Members //////////////////////////
 router.post('/addMember',AuthMiddleware,viewer,check_session,uploadImages,resizeImages,makeMember)
