d37d41bd1b367a95fca6694c00bd50bb94604fa2
DanielDafnis <danieldafnis@gmail.com>
2023-04-14 01:06:42 +0300

    removed auth middleware from public routes

--- api/routes/api_posts.js
+++ api/routes/api_posts.js
@@ -4,7 +4,7 @@ const postController = require("../controllers/api_posts")
 const AuthMiddleware = require("../middleware/api_auth")
 const {admin, viewer, editor} = require("../middleware/api_roles")
 
-router.get("/:id", AuthMiddleware,viewer,postController.find_post)
-router.get("/latest", AuthMiddleware, viewer, postController.get_latest_posts)
+router.get("/:id",postController.find_post)
+router.get("/latest",postController.get_latest_posts)
 
 module.exports = router
