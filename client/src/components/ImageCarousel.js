1b1d305243d92766069d1f32613eb4ef11af2786
DanielDafnis <danieldafnis@gmail.com>
2023-04-20 22:04:38 +0300

    chaining again

--- client/src/components/ImageCarousel.js
+++ client/src/components/ImageCarousel.js
@@ -166,7 +166,7 @@ function ImageCarousel(props) {
   infinite={true}
   className='project-carousel'
   >
-    {Array(5).fill(data.Item?.map(project => <Suspense fallback={<div>Loading . . .</div>}><Card>{project}</Card></Suspense>))}
+    {Array(5).fill(data?.Item?.map(project => <Suspense fallback={<div>Loading . . .</div>}><Card>{project}</Card></Suspense>))}
   </Carousel>;
 
 const newsCar =     <Carousel
