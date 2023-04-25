39a078b552a7f20cc7ecb741cfb3f3678be981c1
DanielDafnis <danieldafnis@gmail.com>
2023-04-25 04:10:07 +0300

    l

--- client/src/Pages/Projects.js
+++ client/src/Pages/Projects.js
@@ -28,6 +28,7 @@ function Projects(){
             <h1 className="project-page-title">Projects Page</h1>
             {console.log(data)}
             {console.log(data.Item)}
+            {console.log(data.length)}
             {data.length > 0 && (
               <ImageCarousel className="project-carousel" category="projects" data={data} />
             )}
