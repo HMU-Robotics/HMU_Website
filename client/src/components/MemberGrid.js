7e7d7ab682d6b1fb4fbf84ce5b6254a00face143
DanielDafnis <danieldafnis@gmail.com>
2023-04-27 04:35:06 +0300

    react is so fucking shit holy shit

--- client/src/components/MemberGrid.js
+++ client/src/components/MemberGrid.js
@@ -64,7 +64,7 @@ function MemberGrid(props) {
             {console.log(memberData?.Item)}
             <h2 className='members-box-title'>Members</h2>
             <Grid2 container spacing={4} columns={12} display="flex" alignItems="center">
-                {memberData.Item && memberData.map((member, i) => (
+                {memberData.Item && memberData?.map((member, i) => (
                     <Grid2 xs={12} sm={6} md={4} lg={3} key={i}>
                         <Suspense fallback={<div>Loading...</div>}><MemberCard key={i} academid_id={member?.academid_id} end_date={member?.end_date} first_name={member?.first_name} last_name={member?.last_name} school={member?.school} subscription_date={member?.subscription_date} image={member?.images[0]?.image_url} /></Suspense>
                     </Grid2>
