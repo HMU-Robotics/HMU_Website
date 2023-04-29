f2fc99099efcd8611fa02e977581bf3136e17654
DanielDafnis <danieldafnis@gmail.com>
2023-04-30 01:08:25 +0300

    img testing

--- client/src/components/MemberCard.js
+++ client/src/components/MemberCard.js
@@ -51,7 +51,7 @@ function MemberCard(props) {
         <div className='members-con'>
             {console.log(props.image)}
             {console.log(image())}
-            <img id='members-img' src={image()}/>
+            <img id='members-img' src={image}/>
             <div className='members-body'>
                 <h3 className='members-title'>{last_name} {first_name}</h3>
                 <p className='members-school'>{school()}</p>
