95a6b271f87c9e67e2b027e1a64c24d75e3057c1
DanielDafnis <danieldafnis@gmail.com>
2023-04-30 05:01:23 +0300

    fix?

--- client/src/Pages/Posts.js
+++ client/src/Pages/Posts.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './Posts.css'
 import { useParams } from "react-router-dom";
-import { ReactMarkdown } from "react-markdown";
+import  ReactMarkdown from "react-markdown";
 
 function Posts() {
 
