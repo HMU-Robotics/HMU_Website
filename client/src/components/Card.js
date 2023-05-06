4b0e78ffbb34b770fcf0b742df90601202576013
DanielDafnis <danieldafnis@gmail.com>
2023-05-07 02:11:57 +0300

    fixed multiple images bug

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -11,12 +11,13 @@ function NewsCard(props) {
   const date = date_json?.substring(0,10)
 
   const image = props.img ? `Uploads/posts/${props.img}` : "Media/about.jpg"
+  const singleImage = image.split(',')[0];
 
   return (
     <Link to={`/Post/${post_id}`} className="card-link">
       <div className="card-con">
         {console.log(props.img)}
-        <img id='card-img' src={image}/>
+        <img id='card-img' src={singleImage}/>
         <div className='card-body'>
           <p className='card-title'>{title}</p>
           <p className='card-date'>{date}</p>
