3d05c5c41c8e0afb86e04dcaccb1b43b61691f50
Daniel <danieldafnis@gmail.com>
2023-07-01 22:21:05 +0300

    fixes

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -7,22 +7,48 @@ function News(props){
 
     const [data , setData] = useState([])
 
+    // useEffect(() => {
+    //     fetch(`https://robotics-club.hmu.gr:443/api/posts/find/latest`, {})
+    //     .then((res) => res.json())
+    //     .then((response) => {
+    //       setData(response)
+    //       console.log(`https://robotics-club.hmu.gr:443/api/posts/find/latest`);
+    //       console.log(response)
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //   })
+    // }, [props]);
+
+
+    // Changes of News to show all projects and seminars in carousel instead of news
+
+
+    // fetches simultaneously both projects and seminars , concats into data for it to be iterated in carousel
     useEffect(() => {
-        fetch(`https://robotics-club.hmu.gr:443/api/posts/find/latest`, {})
-        .then((res) => res.json())
-        .then((response) => {
-          setData(response)
-          console.log(`https://robotics-club.hmu.gr:443/api/posts/find/latest`);
-          console.log(response)
+      Promise.all([
+        fetch(`https://robotics-club.hmu.gr:443/api/posts/find/projects`),
+        fetch(`https://robotics-club.hmu.gr:443/api/posts/find/seminars`)
+      ])
+        .then(([projectsResponse, seminarsResponse]) =>
+          Promise.all([projectsResponse.json(), seminarsResponse.json()])
+        )
+        .then(([projectsData, seminarsData]) => {
+          const combinedData = projectsData.concat(seminarsData);
+          setData(combinedData);
+          console.log(`https://robotics-club.hmu.gr:443/api/posts/find/projects`);
+          console.log(`https://robotics-club.hmu.gr:443/api/posts/find/seminars`);
+          console.log(combinedData);
         })
         .catch((error) => {
           console.log(error);
-      })
+        });
     }, [props]);
+
 
     return(
         <div className="new-cont">
-            <h2 className="new-title">News Feed</h2>
+            <h2 className="new-title">Activities</h2>
             <ImageCarousel className="news-carousel" category="news" data={data}></ImageCarousel>
         </div>
     )
