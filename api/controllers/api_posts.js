d7ed99d045bb3475abe0c62bb0001be492dcaf55
DanielDafnis <danieldafnis@gmail.com>
2023-08-07 19:56:30 +0300

    fuck me

--- api/controllers/api_posts.js
+++ api/controllers/api_posts.js
@@ -71,8 +71,14 @@ exports.get_latest_posts = async(req,res,next) => {
 
 // finds all Posts
 exports.get_posts = async(req,res,next) => {
-    const { language } = req.params;
-    const postLanguage = language == "en" ? "post_en" : "post_gr";
+    var language;
+    if(!req.params) {
+        language = null;
+    }
+    else {
+        language = req.params;
+    }
+    const postLanguage = language === "en" ? "post_en" : "post_gr";
     db.execute(`
         SELECT p.*, pi.img
         FROM post p
