83dcf43755fa4ef6eb2fd5de3d2903a39c237aca
DanielDafnis <danieldafnis@gmail.com>
2023-08-07 14:06:55 +0300

    flag responsiveness

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -32,8 +32,8 @@ function Header() {
               <a href='https://www.youtube.com/channel/UChI8Sm95Zt7CYUcYc63ac-w' target="_blank" rel="noopener noreferrer"><svg id='youtube' fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 16 16" width="32px" height="32px"><path d="M 2.488281 3.011719 C 1.664063 3.011719 0.988281 3.6875 0.988281 4.511719 L 0.988281 11.511719 C 0.988281 12.335938 1.664063 13.011719 2.488281 13.011719 L 13.488281 13.011719 C 14.308594 13.011719 14.988281 12.335938 14.988281 11.511719 L 14.988281 4.511719 C 14.988281 3.6875 14.3125 3.011719 13.488281 3.011719 Z M 2.488281 4.011719 L 13.488281 4.011719 C 13.769531 4.011719 13.988281 4.230469 13.988281 4.511719 L 13.988281 11.511719 C 13.988281 11.792969 13.769531 12.011719 13.488281 12.011719 L 2.488281 12.011719 C 2.207031 12.011719 1.988281 11.792969 1.988281 11.511719 L 1.988281 4.511719 C 1.988281 4.230469 2.207031 4.011719 2.488281 4.011719 Z M 6 4.992188 L 6 11.011719 L 11 8 Z M 7 6.757813 L 9.0625 8 L 7 9.242188 Z"/></svg></a>
         </div>
         <div className='flags'>
-          <a onClick={() => setLanguage('english')}><img src='/social/flags/united-kingdom-flag-uk.svg'></img></a>
-          <a onClick={() => setLanguage('greek')}><img src='/social/flags/greece-flag.svg'></img></a>
+          {language === "greek" && <a onClick={() => setLanguage('english')}><img src='/social/flags/united-kingdom-flag-uk.svg'></img></a>}
+          {language === "english" && <a onClick={() => setLanguage('greek')}><img src='/social/flags/greece-flag.svg'></img></a>}
         </div>
       </Nav>
     </Navbar.Collapse>
