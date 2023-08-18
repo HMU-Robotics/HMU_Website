b1957364e5d8fb2bdb5c34c33380599a78d05e2c
TehShadow <74325660+TehShadow@users.noreply.github.com>
2023-08-18 20:31:32 +0300

    api

--- api/routes/api_user.js
+++ api/routes/api_user.js
@@ -8,6 +8,7 @@ const userController = require("../controllers/api_user")
 router.get("/:id",userController.find_user)
 router.get("/find/all",userController.find_all_users)
 router.get('/:id_1/:id_2',userController.find_number_users)
+router.get('/eurobot',userController.find_eurobots_team)
 
 
 module.exports = router
