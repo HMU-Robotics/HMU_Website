b7490d3d7c74c4ecdfad807e6cbfbb546a7668b3
DanielDafnis <danieldafnis@gmail.com>
2023-10-16 18:27:32 +0300

    language based on client ip geolocation

--- client/src/hooks/LanguageContext.js
+++ client/src/hooks/LanguageContext.js
@@ -5,16 +5,28 @@ const LanguageContext = createContext();
 export function LanguageProvider({ children }) {
   const storedLanguage = localStorage.getItem('language') || 'english';
   const [language, setLanguage] = useState(storedLanguage);
+  const [location, setLocation] = useState({});
 
   useEffect(() => {
+    fetch('http://ip-api.com/json')
+      .then(response => response.json())
+      .then(data => setLocation(data))
+      .catch(error => console.error('Error fetching IP data:', error));
+  }, [location]);
+
+
+  useEffect(() => {
+      if(location.country === "Greece") {
+        setLanguage('greek')
+      }
     localStorage.setItem('language', language);
-  }, [language]);
+  }, [language, location]);
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
-}
+} 
 
 export default LanguageContext;
